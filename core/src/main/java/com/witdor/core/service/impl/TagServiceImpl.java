97d14d8085e87ae85c6e0355bdbef14a059aaffb
xiaomo <83387856@qq.com>
2016-09-08 11:14:27 +0800

    添加日期

--- core/src/main/java/com/witdor/core/service/impl/TagServiceImpl.java
+++ core/src/main/java/com/witdor/core/service/impl/TagServiceImpl.java
@@ -48,6 +48,8 @@ public class TagServiceImpl implements TagService {
 
     @Override
     public void add(TagModel model) {
+        model.setCreateTime(new Date());
+        model.setUpdateTime(new Date());
         tagDao.save(model);
     }
 
